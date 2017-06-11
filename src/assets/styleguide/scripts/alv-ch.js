e8fbad9b4f8fa4e1e027ac60d9de9b53bcf427da
Thomas D'Ascoli <thomas@dasco.li>
2017-06-11 20:54:43 +0200

    Add Megamenu-Dropdown

--- src/assets/styleguide/scripts/alv-ch.js
+++ src/assets/styleguide/scripts/alv-ch.js
@@ -4,4 +4,8 @@
 
 'use strict';
 
-
+// todo refactor!
+var positionLeft = $('.nav-item.megamenu').offset().left;
+var marginMegamenu = ($(document).width() - 1540)/2;
+var diff = -(positionLeft - marginMegamenu);
+$('.nav-item.megamenu .dropdown-menu').css('left',diff);
