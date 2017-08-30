bede923771d8348f64059f52bbfd9b4735c6f4d4
Thomas D'Ascoli <thomas@dasco.li>
2017-08-30 21:04:38 +0200

    Add mobile view job search

--- src/assets/styleguide/scripts/alv-ch_reset.js
+++ src/assets/styleguide/scripts/alv-ch_reset.js
@@ -7,17 +7,39 @@
 // Reset CD Bund
 $( document ).ready(function() {
 
-	console.log('ready');
 	var alvHeader = '<div class="alv-header alv-header--inverse bg-inverse">'+
 		'<span class="alv-header__brand mb-0">'+
 		'<img src="https://cdn.rawgit.com/alv-ch/styleguide/6306dee9/build/images/swiss.svg" width="20" height="20" class="d-inline-block" alt="Swiss Confederation">'+
-		'Arbeitslosenversicherung ALV'+
+		'&nbsp;Arbeitslosenversicherung ALV'+
 		'</span>'+
 		'</div>';
 
-	var header = $('header').detach();
+	var header = 	'<div class="header sticky-top">' +
+		'<div class="navbar navbar-alv navbar-light bg-faded">' +
+		'<div class="container"></div>' +
+		'</div>' +
+		'</div>';
 
+	// lang-switcher
+	$('.nav-lang').addClass('nav-item');
+	$('.nav-lang > ul').addClass('navbar-nav').addClass('navbar--language-switcher');
+	$('.nav-lang > ul > li').addClass('nav-item');
+	$('.nav-lang > ul > li > a').addClass('nav-link');
 
-	$('.container.container-main').before(alvHeader);
-	$('.container.container-main').before(header);
+	// main nav
+	$('.nav-main > .navbar-nav > li').addClass('nav-item');
+	$('.nav-main > .navbar-nav > li > a').addClass('nav-link');
+	$('.nav-main').removeClass('navbar');
+	var navi = $('.mod-mainnavigation').detach();
+
+	$('.container-main').removeClass('container');
+	$('.container-fluid').addClass('container');
+
+	$('header').wrap(header);
+	if (!$('.alv-header').length){
+		$('.header').prepend(alvHeader);
+	}
+
+	$('.mod-searchfield').after(navi);
+
 });
