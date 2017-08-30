7269c89d91b025c77b4b8e99f148b049d19fa3f7
Thomas D'Ascoli <thomas@dasco.li>
2017-08-30 21:24:53 +0200

    Add mobile search criteria functionality

--- src/assets/styleguide/scripts/alv-ch.js
+++ src/assets/styleguide/scripts/alv-ch.js
@@ -11,7 +11,6 @@ var marginMegamenu = ($(document).width() - 1540)/2;
 var diff = -(positionLeft - marginMegamenu);
 $('.nav-item.megamenu .dropdown-menu').css('left',diff);
 
-
 $( document ).ready(function() {
 
 	var md = new MobileDetect(window.navigator.userAgent);
@@ -25,6 +24,17 @@ $( document ).ready(function() {
 
 		$('.toolbar-nav > .nav-item').click(function(){
 			$(this).parent().removeClass('show');
+		});
+	}
+	else {
+		/*
+		 * STICKIES:
+		 * .sticked--top = fixed dom-top
+		 * .sticked--bottom = fixed dom-bottom
+		 */
+		$('.sticked--top').each(function(){
+			$(this).after('<div class="sticked--container" style="height: '+$(this).outerHeight(true)+'px"></div>');
+			$(this).css('top',$(this).offset().top);
 		});
 	}
 
