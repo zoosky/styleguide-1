48d731b8affe1febc4231614fa846b52c67dd7b6
Thomas D'Ascoli <thomas@dasco.li>
2017-08-30 18:30:42 +0200

    Fix #1 footer is sticky

--- src/assets/styleguide/scripts/alv-ch.js
+++ src/assets/styleguide/scripts/alv-ch.js
@@ -5,49 +5,28 @@
 'use strict';
 
 // todo refactor!
-/*
+
 var positionLeft = $('.nav-item.megamenu').offset().left;
 var marginMegamenu = ($(document).width() - 1540)/2;
 var diff = -(positionLeft - marginMegamenu);
 $('.nav-item.megamenu .dropdown-menu').css('left',diff);
-*/
 
-// Reset CD Bund
+
 $( document ).ready(function() {
 
-	var alvHeader = '<div class="alv-header alv-header--inverse bg-inverse">'+
-		'<span class="alv-header__brand mb-0">'+
-		'<img src="https://cdn.rawgit.com/alv-ch/styleguide/6306dee9/build/images/swiss.svg" width="20" height="20" class="d-inline-block" alt="Swiss Confederation">'+
-		'&nbsp;Arbeitslosenversicherung ALV'+
-		'</span>'+
-		'</div>';
+	var md = new MobileDetect(window.navigator.userAgent);
 
-	var header = 	'<div class="header sticky-top">' +
-						'<div class="navbar navbar-alv navbar-light bg-faded">' +
-							'<div class="container"></div>' +
-						'</div>' +
-					'</div>';
+	if (md.mobile()){
+		console.log('mobile true');
 
-	// lang-switcher
-	$('.nav-lang').addClass('nav-item');
-	$('.nav-lang > ul').addClass('navbar-nav').addClass('navbar--language-switcher');
-	$('.nav-lang > ul > li').addClass('nav-item');
-	$('.nav-lang > ul > li > a').addClass('nav-link');
+		$('.toolbar-nav > .nav-title').click(function(){
+			$(this).parent().toggleClass('show');
+		});
 
-	// main nav
-	$('.nav-main > .navbar-nav > li').addClass('nav-item');
-	$('.nav-main > .navbar-nav > li > a').addClass('nav-link');
-	$('.nav-main').removeClass('navbar');
-	var navi = $('.mod-mainnavigation').detach();
-
-	$('.container-main').removeClass('container');
-	$('.container-fluid').addClass('container');
-
-	$('header').wrap(header);
-	if (!$('.alv-header').length){
-		$('.header').prepend(alvHeader);
+		$('.toolbar-nav > .nav-item').click(function(){
+			$(this).parent().removeClass('show');
+		});
 	}
 
-	$('.mod-searchfield').after(navi);
-
 });
+
